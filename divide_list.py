c0763652d2c26ef338467949d6a69868d9aa9ef9
Lukas Leščinskas <71329803+Lukas-Les@users.noreply.github.com>
2020-12-02 12:56:20 +0200

    Update and rename first_script.py to divide_list.py
    This loop will divide a list into smaller lists. I needed it to divide a huge list of emails into small parts, because I couldn't send them all at the same time.

---
+++ divide_list.py
@@ -0,0 +1,4 @@
+my_list = ['apple', 'orange', 'cherry', 'strawberry', 'banana', 'grape', 'peach', 'peear', 'pineapple', 'melon', 'watermelon']
+
+for i in range(0, len(my_list), 3):
+	print(my_list[i:i + 3])
--- first_script.py
+++
@@ -1,6 +0,0 @@
-my_list = ['geeks', 'for', 'geeks', 'like', 
-           'geeky','nerdy', 'geek', 'love', 
-               'questions','words', 'life'] 
-
-for i in range(0, len(my_list), 3):
-	print(my_list[i:i + 3])
